f0be37b8b6dd54b34eec6e9684d338a2b5903fd1
Eric Johnson <ericjohnson0216@icloud.com>
2019-08-30 19:25:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,8 @@ def assign_rooms(attendees)
   end
 end
 
-def printer(atendees)
-  badge_maker
-
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
 end
